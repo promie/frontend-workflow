615ae9ea5cbed79d22d48733ca297d5d0d567494
Promie Yutasane <promie.yutasane@gmail.com>
2023-04-18 16:47:12 +1000

    added comments to the ts-ignore as per the eslint rule

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  // @ts-ignore
+  // @ts-ignore: to address the types later
   test: {
     globals: true,
     environment: 'jsdom',
