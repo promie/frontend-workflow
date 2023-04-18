257ec3f55fb1285739ea454ff2deac5cf7f4f50c
Promie Yutasane <promie.yutasane@gmail.com>
2023-04-19 07:36:09 +1000

    FW-08 configure the vite config to build folder and open default on port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+  },
   // @ts-ignore: to address the types later
   test: {
     globals: true,
