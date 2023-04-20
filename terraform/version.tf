f34ec63bdb4ea0dada92797bfd53ba4cd928395f
Promie Yutasane <promie.yutasane@gmail.com>
2023-04-20 11:35:01 +1000

    FW-15 applying terraform init and added the main file

--- terraform/version.tf
+++ terraform/version.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.2.0, <= 1.4.5"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
